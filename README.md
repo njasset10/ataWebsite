dc49362e6786f82b807aba6d834fe8208570d13c
Nicholas Jasset <nick>
2018-11-12 18:55:54 -0500

    odd

--- README.md
+++
@@ -1,4 +0,0 @@
-# pitchAppSGB
-This is the app that people will be able to use to love Astrid and Eli
-
-Hilary is so so cool.
--- src/components/common/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <div>
-    Made with love by Da Stookie Bradas LLC
-  </div>
-);
-
-export default Footer;
